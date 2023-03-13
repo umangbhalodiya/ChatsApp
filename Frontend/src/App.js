0d865843f3d75bebe5a514292f413be5aa1cf71c
umangbhalodiya <umangbhalodiya660@gmail.com>
2023-03-13 17:17:46 +0530

    ur changed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Components/Chat";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("http://13.233.152.235:3001");
 
 function App() {
   const [username, setUsername] = useState("");
